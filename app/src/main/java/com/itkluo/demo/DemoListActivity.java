3b5eb1b410a341c8488f5f4b744c9637d6e9e0f8
kluo <1156614184@qq.com>
2019-03-20 11:08:28 +0800

    rxjava demo

--- app/src/main/java/com/itkluo/demo/DemoListActivity.java
+++ app/src/main/java/com/itkluo/demo/DemoListActivity.java
@@ -37,7 +37,7 @@ public class DemoListActivity extends AppCompatActivity {
         mainLayout = findViewById(R.id.mainLayout);
         String[] values = {"使用Binder进行IPC通信", "使用AIDL进行IPC通信", "图片轮播", "ViewPage列表中gridview", "下拉级联菜单", "点击箭头显示下拉菜单", "ConstraintLayout嵌套在ScrollView里面"
                 , "CoordinatorLayout嵌套滑动", "CoordinatorLayout嵌套ListView", "可扩展收缩的FlowLayout", "过度绘制布局(设置/辅助功能/开发者选项/，打开调试GPU过度绘制选项）", "内存MAT分析",
-                "伸缩TextView--CollapsibleTextView", "测试 Demo", "改造系统TabLayout", "抢购倒计时", "商品规格选择弹窗", "点击右上角弹出下拉菜单"};
+                "伸缩TextView--CollapsibleTextView", "测试 Demo", "改造系统TabLayout", "抢购倒计时", "商品规格选择弹窗", "点击右上角弹出下拉菜单", "RxJava操作符"};
         //List<String> list = Arrays.asList(values);
         //Arrays.asList(values)返回的是一个只读的List，不能进行add和remove
         //new ArrayList<>(Arrays.asList(values))则是一个可写的List，可以进行add和remove
@@ -103,6 +103,9 @@ public class DemoListActivity extends AppCompatActivity {
                         break;
                     case 17:
                         DemoListActivity.this.startActivity(new Intent(DemoListActivity.this, TitleDownMenuActivity.class));
+                        break;
+                    case 18:
+                        DemoListActivity.this.startActivity(new Intent(DemoListActivity.this, RxJavaDemoAct.class));
                         break;
                     default:
                         break;
---
+++ app/src/main/java/com/itkluo/demo/RxJavaDemoAct.java
@@ -0,0 +1,128 @@
+package com.itkluo.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+
+/**
+ * Created by luobingyong on 2019/3/20.
+ */
+public class RxJavaDemoAct extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_rxjava_demo);
+
+        //以下是flatMap、concatMap和switchMap的运行实例对比：
+        findViewById(R.id.textView).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //flatMap操作符的运行结果
+                flatMapTest();
+            }
+        });
+        findViewById(R.id.textView2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //concatMap操作符的运行结果
+                concatMapTest();
+            }
+        });
+        findViewById(R.id.textView3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //switchMap操作符的运行结果
+                switchMapTest();
+            }
+        });
+
+    }
+
+    public void switchMapTest() {
+//        Observable.just(100, 2000, 3000).switchMap(new Function<Integer, Observable<Integer>>() {
+//            @Override
+//            public Observable<Integer> apply(Integer integer) throws Exception {
+//                //100的延迟执行时间为200毫秒、2000和3000的延迟执行时间为180毫秒
+//                int delay = 200;
+//                if (integer == 2000 || integer == 3000) {
+//                    delay = 180;
+//                }
+//
+//                return Observable.fromArray(new Integer[]{integer, integer / 2}).delay(delay, TimeUnit.MILLISECONDS);
+//            }
+//        }).observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<Integer>() {
+//            @Override
+//            public void accept(Integer integer) throws Exception {
+//                System.out.println("switchMap Next:" + integer);
+//            }
+//        });
+
+        Observable.just(100, 2000, 3000).switchMap(new Function<Integer, Observable<Integer>>() {
+            @Override
+            public Observable<Integer> apply(Integer integer) throws Exception {
+                //100的延迟执行时间为200毫秒、2000和3000的延迟执行时间为180毫秒
+                int delay = 200;
+                if (integer == 2000 || integer == 3000) {
+                    delay = 180;
+                }
+
+                return Observable.just(integer).delay(delay, TimeUnit.MILLISECONDS);
+            }
+        }).observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+                System.out.println("switchMap Next:" + integer);
+            }
+        });
+    }
+
+    private void concatMapTest() {
+        Observable.just(100, 2000, 3000).concatMap(new Function<Integer, Observable<Integer>>() {
+            @Override
+            public Observable<Integer> apply(Integer integer) throws Exception {
+                //100的延迟执行时间为200毫秒、2000和3000的延迟执行时间为180毫秒
+                int delay = 200;
+                if (integer == 2000 || integer == 3000) {
+                    delay = 180;
+                }
+
+                return Observable.fromArray(new Integer[]{integer, integer / 2}).delay(delay, TimeUnit.MILLISECONDS);
+            }
+        }).observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+                System.out.println("concatMap Next:" + integer);
+            }
+        });
+    }
+
+    private void flatMapTest() {
+        Observable.just(100, 2000, 3000).flatMap(new Function<Integer, Observable<Integer>>() {
+            @Override
+            public Observable<Integer> apply(Integer integer) throws Exception {
+                //100的延迟执行时间为200毫秒、2000和3000的延迟执行时间为180毫秒
+                int delay = 200;
+                if (integer == 2000 || integer == 3000) {
+                    delay = 180;
+                }
+
+                return Observable.fromArray(new Integer[]{integer, integer / 2}).delay(delay, TimeUnit.MILLISECONDS);
+            }
+
+        }).observeOn(AndroidSchedulers.mainThread()).subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+                System.out.println("flatMap Next:" + integer);
+            }
+        });
+    }
+}
+
