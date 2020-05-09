4eb6850640c92f047ac642a985516f5f3e1cd6b8
luoby <1156614184@qq.com>
2020-05-09 21:40:17 +0800

    TestFile

---
+++ app/src/main/java/com/itkluo/demo/TestGit.java
@@ -0,0 +1,13 @@
+package com.itkluo.demo;
+
+/**
+ * @author luobingyong
+ * @date 2020/5/9
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        int i;
+        //测试用例
+        TestGit testGit = new TestGit();
+    }
+}
